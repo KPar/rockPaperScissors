1f36dfcba7132d4ecf95cd0f3780675089cc9b99
Kenny P <kenparedes27@gmail.com>
2021-08-25 21:08:56 -0400

    Added basic UI

--- script.js
+++ script.js
@@ -5,17 +5,9 @@ function computerPlay () {
 
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
-    let winner = "";
     if (playerSelection === computerSelection){
-        console.log("It's a tie!");
         return "tie";
     } 
-
-    if (playerSelection !== "rock" && playerSelection !== "scissors" 
-        && playerSelection !== "paper") {
-        console.log("Invalid input");
-        return "invalid";
-    }
 
     if((playerSelection === "rock" && computerSelection === "scissors") 
         || (playerSelection === "paper" && computerSelection === "rock") 
@@ -26,10 +18,64 @@ function playRound(playerSelection, computerSelection) {
     }
  
 }
+const button = document.querySelectorAll("button");
+const results = document.querySelector("div");
 
 function game(){
     let playerScore = 0;
     let computerScore = 0;
+
+    button.forEach(button => button.addEventListener("click", (e) => 
+    {
+        let playerSelection = "";
+
+        switch(e.target.getAttribute("data-button")){
+            case "rock":
+                playerSelection = "rock"
+                break;
+            case "paper":
+                playerSelection = "paper"
+                break;
+            case "scissors":
+                playerSelection = "scissors"
+                break;
+            default:
+                console.log("cool");
+        }
+        const computerSelection = computerPlay();
+        let winner = playRound(playerSelection, computerSelection);
+        if(winner === "player"){
+            playerScore++;
+            results.textContent = `You win! ${playerSelection} beats ${computerSelection}! Score: ${playerScore}:${computerScore}`;
+        }else if(winner === "computer"){
+            computerScore++;
+            results.textContent = `You lose! ${computerSelection} beats ${playerSelection}! Score: ${playerScore}:${computerScore}`;
+        } else {
+            results.textContent = "It's a tie!";
+        }
+        if(playerScore === 2){
+            results.textContent = `You Win the Game! Score: ${playerScore}:${computerScore}`;
+            playerScore = 0;
+            computerScore = 0;
+        }else if (computerScore === 2){
+            results.textContent = `You Lose the Game! Score: ${playerScore}:${computerScore}`;
+            playerScore = 0;
+            computerScore = 0;
+        }
+        /*
+        if(playerScore === computerScore){
+            return `It's a tie! Score: ${playerScore}:${computerScore}`;
+        }else if(Math.max(playerScore, computerScore) === playerScore){
+            return `You Win! Score: ${playerScore}:${computerScore}`;
+        }else{
+            return `You Lose! Score: ${playerScore}:${computerScore}`;
+        }*/
+        //write the game code here to start 
+        //each time user presses a button, we have computerplay then playRound then add the appropriate score,
+        //first player to 5 for any , we end game nd annoucne winner
+
+    }))
+/*
     for (let i = 0; i < 5; i++){
         console.log(i);
         const playerSelection = prompt("Let's Play! Choose Rock, Paper, or Scissors!");
@@ -49,7 +95,7 @@ function game(){
         return `You Win! Score: ${playerScore}:${computerScore}`;
     }else{
         return `You Lose! Score: ${playerScore}:${computerScore}`;
-    }
-}
+    }*/
+} 
 
 console.log(game());
